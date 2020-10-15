4c149a2e0816eff8e2ce299c7ee96f2134339617
youkidearitai <youkidearitai@gmail.com>
2020-10-15 23:34:33 +0900

    parse { }

--- json-python.py
+++ json-python.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
 
+class MyJsonParseError(Exception):
+  pass
+
 class MyJson():
 
   def parse(self, string):
-    self.strings = list(string)
+    self.strings = iter(string)
     self.index = 0
 
-    for string in self.strings:
-      self.whitespace(string)
+    first = next(self.strings)
 
-  def whitespace(self, string):
-    if (string in [" ", "\n", "\r", "\t"]):
+    if first != '{':
+      raise MyJsonParseError(f'Invalid first word {first}')
+
+    try:
+      while True:
+        word = next(self.strings)
+        self.whitespace(word)
+    except StopIteration:
+      if word != '}':
+        raise MyJsonParseError(f'Invalid end word {word}')
+
+  def whitespace(self, word):
+    if (word in [" ", "\n", "\r", "\t"]):
       self.index += 1
 
 myjson = MyJson()
-myjson.parse("    ")
+myjson.parse("{    }")
 print(myjson.index)
